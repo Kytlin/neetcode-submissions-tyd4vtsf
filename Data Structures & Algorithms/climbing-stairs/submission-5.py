c432f4c938d098264f244aac00aa418a01c9eeb0
Ilia Egorov <70240351+Kytlin@users.noreply.github.com>
2026-05-18 21:45:08 -0400

    Add: climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/climbing-stairs/submission-5.py
@@ -0,0 +1,12 @@
+from functools import lru_cache
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+
+        @lru_cache
+        def dfs(i):
+            if i == 0 or i == 1:
+                return 1
+            return dfs(i-1) + dfs(i-2)
+
+        return dfs(n)
