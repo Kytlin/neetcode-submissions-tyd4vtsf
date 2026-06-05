7ad4a1770bc22fc4e103f5e763ca551e53711339
Ilia Egorov <70240351+Kytlin@users.noreply.github.com>
2026-06-05 16:35:39 -0400

    Add: count-connected-components - submission-1

---
+++ Data Structures & Algorithms/count-connected-components/submission-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        adj_list, count = defaultdict(set), 0
+        used_vertices = set()
+        for edge in edges:
+            used_vertices.add(edge[0])
+            used_vertices.add(edge[1])
+            adj_list[edge[0]].add(edge[1])
+            adj_list[edge[1]].add(edge[0])
+
+        visited = [0] * n
+
+        def dfs(cur_vrtx):
+            for next_vrtx in adj_list[cur_vrtx]:
+                if not visited[next_vrtx]:
+                    visited[next_vrtx] = 1
+                    dfs(next_vrtx)
+        
+        for i in range(n):
+            if not visited[i]:
+                visited[i] = 1
+                dfs(i)
+                count += 1
+        return count
